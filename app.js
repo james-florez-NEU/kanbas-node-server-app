2699928bd4dc7e18899b4d476db7273884386e9b
James <jamesflorez11@gmail.com>
2023-11-13 18:15:13 -0800

    A5: Up to 5.2

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from "express";
+import "dotenv/config";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
