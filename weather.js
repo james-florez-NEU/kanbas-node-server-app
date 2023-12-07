b774cd32224ed8f1142aa1cbae3f9d3575ffa0b4
James <jamesflorez11@gmail.com>
2023-12-06 18:09:38 -0800

    A6: change url to api/weather

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const Weather = (app) => {
-    app.get('/weather', async (req, res) => {
+    app.get('/api/weather', async (req, res) => {
         try {
-            const response = await axios.get("{WEATHER_API_BASE_AND_KEY}&q=London");
+            const response = await axios.get("http://api.weatherapi.com/v1/current.json?key=19573b9221824c89a94220852231511&q=London");
             const weatherData = response.data;
             res.json(weatherData);
         } catch (error) {
             res.status(500).json({ error: "current error"});
         }
     });
-    app.get('/weather/search', async (req, res) => {
+    app.get('/api/weather/search', async (req, res) => {
         try {
             const response = await axios.get("{WEATHER_API_BASE}/search.json?key={WEATHER_API_KEY}&q=London");
             const weatherData = response.data;
