b105a7b01a61ea0516f59e82820433503e398048
James <jamesflorez11@gmail.com>
2023-12-06 17:41:04 -0800

    A6: add search to weather.js

--- weather.js
+++ weather.js
@@ -7,7 +7,16 @@ const Weather = (app) => {
             const weatherData = response.data;
             res.json(weatherData);
         } catch (error) {
-            res.status(500).json({ error: 'Unable to fetch weather data' });
+            res.status(500).json({ error: "current error"});
+        }
+    });
+    app.get('/weather/search', async (req, res) => {
+        try {
+            const response = await axios.get("{WEATHER_API_BASE}/search.json?key={WEATHER_API_KEY}&q=London");
+            const weatherData = response.data;
+            res.json(weatherData);
+        } catch (error) {
+            res.status(500).json({ error: error.toString() });
         }
     });
 
