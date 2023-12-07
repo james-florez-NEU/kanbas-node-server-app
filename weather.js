d553236bf63ec21f993a75722a333af3bc50907a
James <jamesflorez11@gmail.com>
2023-12-06 19:53:33 -0800

    A6: fixed string concatenation and use of .env file

--- weather.js
+++ weather.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
+
+const WEATHER_API_BASE = process.env.WEATHER_API_BASE;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const Weather = (app) => {
     app.get('/api/weather', async (req, res) => {
@@ -10,9 +14,23 @@ const Weather = (app) => {
             res.status(500).json({ error: "current error"});
         }
     });
-    app.get('/api/weather/search', async (req, res) => {
+    app.get('/api/weather/current', async (req, res) => {
+        const api_string = WEATHER_API_BASE + 'current.json?key=' + WEATHER_API_KEY + '&q=London';
+        console.log(api_string);
         try {
-            const response = await axios.get("{WEATHER_API_BASE}/search.json?key={WEATHER_API_KEY}&q=London");
+
+            const response = await axios.get(api_string);
+
+            const weatherData = response.data;
+            res.json(weatherData);
+        } catch (error) {
+            res.status(500).json({ error: "current error"});
+        }
+    });
+    app.get('/api/weather/search', async (req, res) => {
+        const api_string = WEATHER_API_BASE + 'search.json?key=' + WEATHER_API_KEY + '&q=London';
+        try {
+            const response = await axios.get(api_string);
             const weatherData = response.data;
             res.json(weatherData);
         } catch (error) {
