05555d837ddc91bcb49b05569dc53e6d0795d588
James <jamesflorez11@gmail.com>
2023-12-06 16:47:55 -0800

    A6: change roles in user schema

--- users/schema.js
+++ users/schema.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
         dob: Date,
         role: {
             type: String,
-            enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
+            enum: ["USER", "FORECASTER", "MODERATOR"],
             default: "USER" },
     },
     { collection: "users" });
